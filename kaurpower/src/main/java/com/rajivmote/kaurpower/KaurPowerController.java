1ac2fe101777173e99844c57c410eece0d6eef8a
Rajiv <Rajiv@DESKTOP-J885OO2>
2018-07-12 22:15:03 -0500

    Add Spring Boot tests, split application to Controller

--- kaurpower/src/main/java/com/rajivmote/kaurpower/KaurPowerController.java
+++ kaurpower/src/main/java/com/rajivmote/kaurpower/KaurPowerController.java
@@ -1,5 +1,6 @@
 package com.rajivmote.kaurpower;
 
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,16 @@ public class KaurPowerController {
 	@RequestMapping("/")
 	public String ping() {
 		return "Pong from KaurPowerController";
+	}
+	
+	@RequestMapping("/nextTweets")
+	public Tweet[] getNextTweets(long sinceId) {
+		return null;
+	}
+	
+	@RequestMapping("/getPoemLines")
+	public String[] getPoemLines(Tweet tweet) {
+		return null;
 	}
 
 }
